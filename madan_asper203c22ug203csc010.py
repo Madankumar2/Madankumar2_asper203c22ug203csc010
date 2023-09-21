092635a3198d69e7ea825a39b1745d7759c32330
Madankumar2 <144319859+Madankumar2@users.noreply.github.com>
2023-09-21 12:39:58 +0530

    Add files via upload

--- madan_asper203c22ug203csc010.py
+++ madan_asper203c22ug203csc010.py
@@ -1,119 +1,55 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+User_Id="##@@"
+Password=123
+print("Welcome To TN_BANK")
+print("Enter Your Credentials To Login:")
+Enter_Id=input("Enter User_Id:")
+Enter_Pass=int(input("Enter User Password:"))
 
-= RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py
-Welcome To TN_BANK
-Enter Your Credentials To Login:
-Enter User_Id:##@@
-Enter User Password:123
-Invalid User_id and Password
-gonna kick you
-I gonna make you work...hahaha..
-To Run The Code...
-(bank._customer()
-bank._transaction())
-call the above function
-Traceback (most recent call last):
-  File "C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py", line 48, in <module>
-    bank._customer(321,"Tamil",1000)
-NameError: name 'bank' is not defined
+if(User_Id==Enter_Id and Password==Enter_Pass):
 
-===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
-Welcome To TN_BANK
-Enter Your Credentials To Login:
-Enter User_Id:##@@
-Enter User Password:123
-Invalid User_id and Password
-gonna kick you
-I gonna make you work...hahaha..
-To Run The Code...
-(bank._customer()
-bank._transaction())
-call the above function
-Traceback (most recent call last):
-  File "C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py", line 48, in <module>
-    bank._customer(321,"Tamil",1000)
-NameError: name 'bank' is not defined
+    class Bank_Account():
+        Bank_Name="TN_BANK"
+        Branch="TamilNadu"
+        def _customer(self,acc_no,name,balance):
+            self.acc_no=acc_no
+            print("Customer's Account Number:",self.acc_no)
+            self.name=name
+            print("Account_Holder's Name:",self.name)
+            self.balance=balance
+            print("Clear Balance:",balance)
 
-===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
-Welcome To TN_BANK
-Enter Your Credentials To Login:
-Enter User_Id:##@@
-Enter User Password:123
-Invalid User_id and Password
-gonna kick you
-I gonna make you work...hahaha..
-To Run The Code...
-(bank._customer()
-bank._transaction())
-call the above function
-Traceback (most recent call last):
-  File "C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py", line 48, in <module>
-    bank=Bank_Account()
-NameError: name 'Bank_Account' is not defined
+    
+        def _transaction(self,deposit,withdraw,balance):              
+            self.deposit=deposit
+            self.withdraw=withdraw
+            Enter_dep=float(input("Enter Amount in INR To Add:"))
+            if(Enter_dep>=1):
+                self.deposit=deposit+Enter_dep
+                print(self.deposit)
+                print("Now your avail Balance:",self.deposit)
+                self.deposit=balance    
+            Enter_with=float(input("Enter Amount in INR To Withdraw:"))
+            if(Enter_with>=0):
+                self.withdraw=balance-Enter_with
+                print(self.withdraw)
+                print("Your Available Balance is:",self.withdraw)
+            else:
+                return Bank_Account._customer(321,"Tamil",1000)
 
-===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
-Welcome To TN_BANK
-Enter Your Credentials To Login:
-Enter User_Id:
-===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
-Welcome To TN_BANK
-Enter Your Credentials To Login:
-Enter User_Id:##@@
-Enter User Password:123
-Customer's Account Number: 321
-Account_Holder's Name: Tamil
-Clear Balance: 1000
-If you want to deposit then enter the amount,else let you be remind as 0
-If you want to withdraw then enter the amount,else let you be remind as 0
-Enter Amount in INR To Add:1000
-2000.0
-Enter Amount in INR To Withdraw:120
-1000
+            
+else:
+    print("Invalid User_id and Password")
 
-===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
-Welcome To TN_BANK
-Enter Your Credentials To Login:
-Enter User_Id:##@@
-Enter User Password:123
-Customer's Account Number: 321
-Account_Holder's Name: Tamil
-Clear Balance: 1000
-If you want to deposit then enter the amount,else let you be remind as 0
-If you want to withdraw then enter the amount,else let you be remind as 0
-Enter Amount in INR To Add:0
-Enter Amount in INR To Withdraw:1000
-0.0
->>> 
-===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
-Welcome To TN_BANK
-Enter Your Credentials To Login:
-Enter User_Id:##@@
-Enter User Password:123
-Customer's Account Number: 321
-Account_Holder's Name: Tamil
-Clear Balance: 1000
-If you want to deposit then enter the amount,else let you be remind as 0
-If you want to withdraw then enter the amount,else let you be remind as 0
-Enter Amount in INR To Add:1000
-2000.0
-Enter Amount in INR To Withdraw:500
-500.0
-Your Available Balance is: 1000
->>> 
-===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
-Welcome To TN_BANK
-Enter Your Credentials To Login:
-Enter User_Id:##@@
-Enter User Password:123
-Customer's Account Number: 321
-Account_Holder's Name: Tamil
-Clear Balance: 1000
-If you want to deposit then enter the amount,else let you be remind as 0
-If you want to withdraw then enter the amount,else let you be remind as 0
-Enter Amount in INR To Add:10
-1010.0
-Now your avail Balance: 1000
-Enter Amount in INR To Withdraw:12
-988.0
-Your Available Balance is: 1000
+    print("gonna kick you")
+    print("I gonna make you work...hahaha..")
+    print("To Run The Code...")
+    print("(bank._customer()")
+    print("bank._transaction())")
+    print("call the above function")
+    exit     
+bank=Bank_Account()
+bank=Bank_Account()
+bank._customer(321,"Tamil",1000)
+print("""If you want to deposit then enter the amount,else let you be remind as 0""")
+print("""If you want to withdraw then enter the amount,else let you be remind as 0""")
+bank._transaction(1000,0,1000)              
