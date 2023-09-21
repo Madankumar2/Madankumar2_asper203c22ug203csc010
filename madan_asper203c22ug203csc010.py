473f48b9e83df88bacc1053689c5ffad560ccce7
Madankumar2 <144319859+Madankumar2@users.noreply.github.com>
2023-09-21 12:31:03 +0530

    Add files via upload

---
+++ madan_asper203c22ug203csc010.py
@@ -0,0 +1,119 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py
+Welcome To TN_BANK
+Enter Your Credentials To Login:
+Enter User_Id:##@@
+Enter User Password:123
+Invalid User_id and Password
+gonna kick you
+I gonna make you work...hahaha..
+To Run The Code...
+(bank._customer()
+bank._transaction())
+call the above function
+Traceback (most recent call last):
+  File "C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py", line 48, in <module>
+    bank._customer(321,"Tamil",1000)
+NameError: name 'bank' is not defined
+
+===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
+Welcome To TN_BANK
+Enter Your Credentials To Login:
+Enter User_Id:##@@
+Enter User Password:123
+Invalid User_id and Password
+gonna kick you
+I gonna make you work...hahaha..
+To Run The Code...
+(bank._customer()
+bank._transaction())
+call the above function
+Traceback (most recent call last):
+  File "C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py", line 48, in <module>
+    bank._customer(321,"Tamil",1000)
+NameError: name 'bank' is not defined
+
+===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
+Welcome To TN_BANK
+Enter Your Credentials To Login:
+Enter User_Id:##@@
+Enter User Password:123
+Invalid User_id and Password
+gonna kick you
+I gonna make you work...hahaha..
+To Run The Code...
+(bank._customer()
+bank._transaction())
+call the above function
+Traceback (most recent call last):
+  File "C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py", line 48, in <module>
+    bank=Bank_Account()
+NameError: name 'Bank_Account' is not defined
+
+===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
+Welcome To TN_BANK
+Enter Your Credentials To Login:
+Enter User_Id:
+===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
+Welcome To TN_BANK
+Enter Your Credentials To Login:
+Enter User_Id:##@@
+Enter User Password:123
+Customer's Account Number: 321
+Account_Holder's Name: Tamil
+Clear Balance: 1000
+If you want to deposit then enter the amount,else let you be remind as 0
+If you want to withdraw then enter the amount,else let you be remind as 0
+Enter Amount in INR To Add:1000
+2000.0
+Enter Amount in INR To Withdraw:120
+1000
+
+===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
+Welcome To TN_BANK
+Enter Your Credentials To Login:
+Enter User_Id:##@@
+Enter User Password:123
+Customer's Account Number: 321
+Account_Holder's Name: Tamil
+Clear Balance: 1000
+If you want to deposit then enter the amount,else let you be remind as 0
+If you want to withdraw then enter the amount,else let you be remind as 0
+Enter Amount in INR To Add:0
+Enter Amount in INR To Withdraw:1000
+0.0
+>>> 
+===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
+Welcome To TN_BANK
+Enter Your Credentials To Login:
+Enter User_Id:##@@
+Enter User Password:123
+Customer's Account Number: 321
+Account_Holder's Name: Tamil
+Clear Balance: 1000
+If you want to deposit then enter the amount,else let you be remind as 0
+If you want to withdraw then enter the amount,else let you be remind as 0
+Enter Amount in INR To Add:1000
+2000.0
+Enter Amount in INR To Withdraw:500
+500.0
+Your Available Balance is: 1000
+>>> 
+===== RESTART: C:\Users\Students\Downloads\Madan_asper203c22ug203csc010.py =====
+Welcome To TN_BANK
+Enter Your Credentials To Login:
+Enter User_Id:##@@
+Enter User Password:123
+Customer's Account Number: 321
+Account_Holder's Name: Tamil
+Clear Balance: 1000
+If you want to deposit then enter the amount,else let you be remind as 0
+If you want to withdraw then enter the amount,else let you be remind as 0
+Enter Amount in INR To Add:10
+1010.0
+Now your avail Balance: 1000
+Enter Amount in INR To Withdraw:12
+988.0
+Your Available Balance is: 1000
